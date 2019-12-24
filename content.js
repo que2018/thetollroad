5021882842c6bebe06ba41c57b1823ff8ee64b47
que2018 <goodiswill2016@gmail.com>
2019-12-24 10:33:25 -0800

    12242019

--- content.js
+++ content.js
@@ -1,8 +1,13 @@
 
-jQuery(document).ready(function(){
-	jQuery("table tr").each(function() {
-		jQuery(this).find('td').each (function() {
-			console.log(jQuery(this).text());
-		});
-	});
+jQuery(document).ready(function() {
+	jQuery("table tr td:nth-child(3)").each(function() {
+		var LP = jQuery(this).text().trim();
+		console.log(LP);
+
+		if(LP != "58572T2") {
+			//jQuery(this).css("background-color", "yellow");
+		
+			jQuery(this).closest('tr').remove();
+		} 		
+	}); 
 });
