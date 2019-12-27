c70e5b61f59b853e5dd0b58ba0fd892b7bbaafc8
que2018 <goodiswill2016@gmail.com>
2019-12-26 18:06:45 -0800

    12262019

--- content.js
+++ content.js
@@ -1,13 +1,33 @@
 
 jQuery(document).ready(function() {
+	var total = 0;
+	
 	jQuery("table tr td:nth-child(3)").each(function() {
 		var LP = jQuery(this).text().trim();
-		console.log(LP);
 
-		if(LP != "58572T2") {
+		
+
+		if(LP != "8GGC577") {
 			//jQuery(this).css("background-color", "yellow");
 		
 			jQuery(this).closest('tr').remove();
-		} 		
+			
+		} else {
+			let fee = jQuery(this).closest('tr').find('td').eq(5).html();
+			
+			let feeRaw = fee.replace('$', '');
+			let feePlus = parseFloat(feeRaw) + 1;
+			let feeFormat = '$' + feePlus;
+			
+			total+= feePlus;
+			
+			jQuery(this).closest('tr').find('td').eq(5).html(feeFormat);
+		}		
 	}); 
+	
+	total = total.toFixed(2);
+	
+	let totalRow = "<tr><td colspan='6' style='font-size: 18px; font-weight: bold; text-align: right; padding-right: 20px;'>$" + total + "</td></tr>"
+
+	jQuery("table tbody").append(totalRow);
 });
